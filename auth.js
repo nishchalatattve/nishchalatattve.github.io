b183f8bfbbe9938e9d0d4d030d1d4ed64a572960
Hercule Poirot <my.email>
2025-03-29 19:48:51 +0000

    implemented banana

--- auth.js
+++ auth.js
@@ -15,6 +15,19 @@ document.addEventListener("DOMContentLoaded", function() {
         script.src = "random_circle.js";
         document.body.appendChild(script);
     }
+    else if (fruit === "banana") {
+        alert("Access granted");
+        // Change the HTML content
+        document.body.innerHTML = `<img class="background-image" src="banana.jpg"/>
+        <div class="center-text">Happy Father's Day!</div>
+        <button>Update</button> 
+        <canvas></canvas>`;
+        
+        // Add the script element programmatically
+        const script = document.createElement("script");
+        script.src = "random_circle.js";
+        document.body.appendChild(script);
+    }
     else {
         document.body.innerHTML = "<h1>Access Denied</h1><p>You must enter the correct fruit to view this page.</p>";
         // Prevent further script execution
